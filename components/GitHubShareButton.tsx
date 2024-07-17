077e789f4cb265f893ed7299261d550c8ca4c29f
aghosh0605 <aghosh0605@gmail.com>
2024-07-17 22:42:38 +0530

    Update: Repo URL

--- components/GitHubShareButton.tsx
+++ components/GitHubShareButton.tsx
@@ -5,7 +5,7 @@ import styles from './styles.module.css'
 export const GitHubShareButton: React.FC = () => {
   return (
     <a
-      href='https://github.com/aghosh0605/portfolio-website.git'
+      href='https://github.com/aghosh0605/blog-aghosh0605'
       target='_blank'
       rel='noopener noreferrer'
       className={styles.githubCorner}
